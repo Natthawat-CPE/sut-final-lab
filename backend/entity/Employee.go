f86dc4b0e514ee8edff6812d3340bc447f43e19e
Natthawat-CPE <aha0037@gmail.com>
2023-02-14 23:15:41 -0800

    Valid: EmployeeID - close #4

--- backend/entity/Employee.go
+++ backend/entity/Employee.go
@@ -8,5 +8,5 @@ type Employee struct {
 	gorm.Model
 	Name       string `valid:"required~Name cannot be blank"`
 	Email      string
-	EmployeeID string
+	EmployeeID string `valid:"matches(^[JMS][0-9]{8}$)~ต้องขึ้นต้นด้วย J หรือ M หรือ S แล้วตามด้วยตัวเลขจำนวน 8 ตัว"`
 }
